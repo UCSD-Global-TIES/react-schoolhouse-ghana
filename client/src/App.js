ac871caa0a62ca3783de3589b163c08230225b68
matteuc <ymc006@ucsd.edu>
2020-01-30 18:47:39 -0800

    pulled changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import { useAuth0 } from "./react-auth0-spa";
+import React, {
+  useEffect,
+  useState
+} from "react";
+import {
+  Route,
+  Switch
+} from "react-router-dom";
+import {
+  useAuth0
+} from "./react-auth0-spa";
 import PrivateRoute from "./components/PrivateRoute";
 import API from './utils/API';
 import "./App.css";
@@ -15,7 +23,11 @@ function App() {
   //   email: "matt@email.com",
   //   picture: "https://link.com"
   // }
-  const { loading, user, isAuthenticated } = useAuth0();
+  const {
+    loading,
+    user,
+    isAuthenticated
+  } = useAuth0();
   const [userInfo, setUserInfo] = useState();
 
   useEffect(() => {
@@ -24,36 +36,56 @@ function App() {
 
   // Retrieve/create userInfo document after Auth0 login
   const updateUser = () => {
-    if(isAuthenticated && user && !loading){
+    if (isAuthenticated && user && !loading) {
       setUserInfo(user);
     }
   };
 
   if (loading) {
-    return (
-      <div className="App text-center">
-        <div className="mt-5">
-          <Loading />
-        </div>
-      </div>
+    return ( <
+      div className = "App text-center" >
+      <
+      div className = "mt-5" >
+      <
+      Loading / >
+      <
+      /div> <
+      /div>
     );
   }
 
-  return (
-      <div className="App">
-        <header>
-          {/* Place navigation bar here */}
-        </header>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <PrivateRoute exact path="/private-page" component={PrivatePage} user={userInfo}/>
-          <Route component={NoMatch} />
-          {/* <PrivateRoute exact path="/socials/:id" component={Social} user={userInfo}/> */}
-          {/* HOW TO ACCESS ROUTE ID */}
-          {/* this.socialId = this.props.match.params.id; */}
+  return ( <
+    div className = "App" >
+    <
+    header > {
+      /* Place navigation bar here */ } <
+    /header> <
+    Switch >
+    <
+    Route exact path = "/"
+    component = {
+      Landing
+    }
+    /> <
+    PrivateRoute exact path = "/private-page"
+    component = {
+      PrivatePage
+    }
+    user = {
+      userInfo
+    }
+    /> <
+    Route component = {
+      NoMatch
+    }
+    /> {
+      /* <PrivateRoute exact path="/socials/:id" component={Social} user={userInfo}/> */ } {
+      /* HOW TO ACCESS ROUTE ID */ } {
+      /* this.socialId = this.props.match.params.id; */ }
 
-        </Switch>
-      </div>
+    <
+    /Switch> <
+    /div>
   );
 }
 
