7fb7edb42393e5fad0e75abde07e47dcac0599dd
matteuc <ymc006@ucsd.edu>
2020-02-06 10:34:35 -0800

    added admin portal pseudocoding

--- client/src/pages/AccountPortal/versions/admin/AdminPortal.jsx
+++ client/src/pages/AccountPortal/versions/admin/AdminPortal.jsx
@@ -6,7 +6,22 @@ function AdminPortal(props) {
 
     return (
         <div style={{display: "flex", width: "100%", height: "100vh"}}>  
-            <div style={{margin: "auto"}}> ADMIN PORTAL </div>
+            <div style={{ margin: "auto" }}> ADMIN PORTAL </div>
+            {/* Add, Delete, Update Classes */}
+            {/* Add, Delete Student/Teacher accounts to classes */}
+            {/* Get, Add, Delete, Update School Announcements */}
+            {/* Get all grades */}
+            {/* Get, Add, Delete grade */}
+            {/* Add, delete accounts */}
+
+            {/* General Layout (menu in side panel, MUI Responsive Drawer) */}
+            {/* https://www.w3schools.com/howto/howto_css_custom_scrollbar.asp */}
+            {/* https://material-ui.com/components/drawers/#responsive-drawer */}
+            
+            {/* Render all school announcements with CRUD buttons */}
+            {/* Render all grades (w/CRUD dialog) with associated classes (w/CRUD dialog) */}
+            {/* Render all accounts (w/ CRUD dialog) */}
+
         </div>    
     );
 }
