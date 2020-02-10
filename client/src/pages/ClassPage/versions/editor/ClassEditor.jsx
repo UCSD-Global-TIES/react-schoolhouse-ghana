11535bb3ca4176f9dae0c47269172b1111d82302
Neve Foresti <neve.foresti@gmail.com>
2020-02-09 18:53:37 -0800

    finally add delete update buttons

--- client/src/pages/ClassPage/versions/editor/ClassEditor.jsx
+++ client/src/pages/ClassPage/versions/editor/ClassEditor.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import "../../../../utils/flowHeaders.min.css";
 import "./main.css";
@@ -9,6 +10,8 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBullhorn } from '@fortawesome/free-solid-svg-icons'
 import CommentIcon from '@material-ui/icons/Comment';
 import { Grid } from "@material-ui/core";
 
@@ -49,11 +52,14 @@ const useStyles = makeStyles(theme => ({
 
 function ClassEditor(props) {
   const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
+  const [checked, setChecked] = useState([0]);
+  const [isDelete, setDelete] = useState([false]);
+  const [isUpdate, setUpdate] = useState([false]);
 
   const handleToggle = value => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
+
 
     if (currentIndex === -1) {
       newChecked.push(value);
@@ -62,12 +68,31 @@ function ClassEditor(props) {
     }
 
     setChecked(newChecked);
+    if (newChecked.length   ==1) {
+      setDelete(false);
+      setUpdate(false);
+    }
+    else if (newChecked.length  === 2) {
+      setDelete(true);
+      setUpdate(true);
+    }
+    else if (newChecked.length > 2) {
+      setDelete(true);
+      setUpdate(false);
+    }
   };
 
 
   return (
     <div style={{ display: "flex", width: "100%", height: "100vh" }}>
       <div>announcements</div>
+      {isDelete == true &&
+        <button id="delButton">Delete</button>
+      }
+      {isUpdate == true &&
+        <button id="updateButton">Update</button>
+      }
+        <button id="updateButton">Add</button>
       <div style={{ margin: "auto" }}>
         <List className={classes.root}>
           {announcements.map(announcement => {
@@ -87,7 +112,7 @@ function ClassEditor(props) {
                 <ListItemText id={labelId} primary={announcement.date} />
                 <ListItemSecondaryAction>
                   <IconButton edge="end" aria-label="comments">
-                    <CommentIcon />
+                    <FontAwesomeIcon icon={faBullhorn} size="sm" />
                   </IconButton>
                 </ListItemSecondaryAction>
               </ListItem>
