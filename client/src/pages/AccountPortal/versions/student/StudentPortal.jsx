cecec54231fb2ab354f46eba7618ef619d564a46
Matt <ymc006@ucsd.edu>
2020-02-19 13:50:09 -0800

    added pagination for announcements and adjusted page colors

--- client/src/pages/AccountPortal/versions/student/StudentPortal.jsx
+++ client/src/pages/AccountPortal/versions/student/StudentPortal.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom"
-import { Grid, Dialog, DialogContent, DialogContentText, DialogTitle, Slide, DialogActions, List, ListItem, ListItemIcon, ListItemText, Typography, ListSubheader, Button, Card, CardActionArea, CardActions, CardContent, CardMedia } from "@material-ui/core";
+import { IconButton, Grid, Dialog, DialogContent, DialogContentText, DialogTitle, Slide, DialogActions, List, ListItem, ListItemIcon, ListItemText, Typography, ListSubheader, Button, ButtonGroup, CardActionArea, CardActions, CardContent, CardMedia, Card } from "@material-ui/core";
 import SchoolIcon from "@material-ui/icons/Apartment";
 import ClassIcon from "@material-ui/icons/Class";
 import moment from "moment";
 import "../../../../utils/flowHeaders.min.css";
 import "./main.css";
+import clsx from "clsx"
+
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos"
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos"
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,6 +26,10 @@ const useStyles = makeStyles(theme => ({
     cardMedia: {
         height: 140,
     },
+    textGlow: {
+        color: "white",
+        textShadow: "2px 2px 7px #787676"
+    }
 }));
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -31,14 +39,18 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 function StudentPortal(props) {
     const classes = useStyles();
-
+    const MAX_ANN = 3; 
 
     // HOOKS 
     // Stores this student's information
     const [studentInfo, setStudentInfo] = useState({});
+
     // Stores current school announcements
-    const [schoolAnnouncements, setSchoolAnnouncements] = useState([]);
-    const [classAnnouncements, setClassAnnouncements] = useState([]);
+    const [classAnnouncements, setClassAnnouncements] = useState([])
+    const [schoolAnnouncements, setSchoolAnnouncements] = useState([])
+
+    const [schoolAnnPageIdx, setSchoolAnnPageIdx] = useState(0);
+    const [classAnnPageIdx, setClassAnnPageIdx] = useState(0);
 
     // If loading, show loading screen
     const [loading, setLoading] = useState(true);
@@ -121,6 +133,13 @@ function StudentPortal(props) {
         props.history.push(`/class/${class_id}`);
     }
 
+    const handlePageChange = (type, direction) => {
+        if(type === 'class') setClassAnnPageIdx(classAnnPageIdx + direction)
+        else if (type === 'school') setSchoolAnnPageIdx(schoolAnnPageIdx + direction)
+
+        
+    }
+
     useEffect(() => {
         setTimeout(() => {
             // Get & set student info
@@ -150,6 +169,7 @@ function StudentPortal(props) {
 
     return (
         <div style={{ display: "flex", width: "100%", marginTop: "5rem" }}>
+            {currentAnnouncement.title && 
             <Dialog
                 open={dialogOpen}
                 TransitionComponent={Transition}
@@ -171,18 +191,28 @@ function StudentPortal(props) {
 
                 </DialogActions>
             </Dialog>
-            <Grid container >
+            
+            }
+            
+            <Grid spacing={5} container style={{padding: "2rem", width:"100%"}}>
+                <Grid item xs={12}><Typography style={{padding: "2rem"}} align='center' className={clsx(classes.textGlow, "flow-text")} variant="h3">welcome back, {props.user.profile.first_name} 😄</Typography> </Grid>
                 {/* Announcements */}
-                <Grid item xs={12} md={4}>
+                <Grid item xs={12} md={5} lg={4} xl={3}>
                     <List
                         className={classes.root}
                         subheader={
-                            <ListSubheader component="div">
-                                School Announcements
+                            <ListSubheader component="div" style={{display: "flex", justifyContent: "space-between"}}>
+                                <span>School Announcements</span>
+                                <ButtonGroup size="small" aria-label="small outlined button group">
+                                <ButtonGroup size="small" aria-label="small outlined button group">
+                                    <IconButton size="small" disabled={schoolAnnPageIdx === 0} onClick={() => handlePageChange('school', -1)}><ArrowBackIosIcon /></IconButton>
+                                    <IconButton size="small" disabled={schoolAnnPageIdx === Math.ceil(schoolAnnouncements.length / MAX_ANN) - 1} onClick={() => handlePageChange('school', 1)}><ArrowForwardIosIcon /></IconButton>
+                                </ButtonGroup>
+                                </ButtonGroup>
                             </ListSubheader>}
                     >
                         {
-                            schoolAnnouncements.map((schoolAnn, idx) => (
+                            schoolAnnouncements.slice(schoolAnnPageIdx * MAX_ANN, (schoolAnnPageIdx + 1) * MAX_ANN).map((schoolAnn, idx) => (
 
 
                                 <ListItem button onClick={() => handleOpenCurrentAnn(schoolAnn)} key={`school-ann-${idx}`} alignItems="flex-start">
@@ -209,12 +239,16 @@ function StudentPortal(props) {
                     <List
                         className={classes.root}
                         subheader={
-                            <ListSubheader component="div">
-                                Class Announcements
+                            <ListSubheader component="div" style={{display: "flex", justifyContent: "space-between"}}>
+                                <span>Class Announcements</span>
+                                <ButtonGroup size="small" aria-label="small outlined button group">
+                                    <IconButton size="small" disabled={classAnnPageIdx === 0} onClick={() => handlePageChange('class', -1)}><ArrowBackIosIcon /></IconButton>
+                                    <IconButton size="small" disabled={classAnnPageIdx === Math.ceil(classAnnouncements.length / MAX_ANN) - 1} onClick={() => handlePageChange('class', 1)}><ArrowForwardIosIcon /></IconButton>
+                                </ButtonGroup>
                             </ListSubheader>}
                     >
                         {
-                            classAnnouncements.map((classAnn, idx) => (
+                            classAnnouncements.slice(classAnnPageIdx * MAX_ANN, (classAnnPageIdx + 1) * MAX_ANN).map((classAnn, idx) => (
 
 
                                 <ListItem button onClick={() => handleOpenCurrentAnn(classAnn)} key={`class-ann-${idx}`} alignItems="flex-start">
@@ -240,12 +274,12 @@ function StudentPortal(props) {
 
                 </Grid>
                 {/* Classes */}
-                <Grid item xs={12} md={8}>
-                    <Grid spacing={1} container>
+                <Grid item xs={12} md={7} lg={8} xl={9}>
+                    <Grid spacing={1} align="center" container>
 
                         {
                             studentInfo.classes.map((classDoc, idx) => (
-                                <Grid onClick={() => handleOpenClass(classDoc._id)} key={`class-card-${idx}`} item xs={12} sm={6} md={4} >
+                                <Grid onClick={() => handleOpenClass(classDoc._id)} key={`class-card-${idx}`} item xs={12} sm={6} md={4} lg={3} >
                                     <Card raised className={classes.card}>
                                         <CardActionArea >
                                             <CardMedia
