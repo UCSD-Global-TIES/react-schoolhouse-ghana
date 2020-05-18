72ad704a574c955758a40540daa7d5c4730f2da4
Daniel Truong <37765758+daniel-d-truong@users.noreply.github.com>
2020-05-17 20:12:27 -0700

    Fixed schema (#24)

--- models/Form.js
+++ models/Form.js
@@ -2,31 +2,25 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const formSchema = new Schema({
-  forms:{
-    questions: [Questions],
-    title: String
+  questions: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Question'
+  }],
+  title: {
+    type: String,
+    required: true
   },
-  questions: {
-    prompt: String,
-    choices: [String],
-    rating: (Int, Int),
-    isFreeResponse: Boolean,
-    response: [Responses]
-  },
-  Response: {
-    question: Questions,
-    answer: String,
-    explanation: String,
-    user: String
+  annonymous: {
+    type: Boolean,
+    required: true
   }
-
 }, { timestamps: true });
 
-const Forms = mongoose.model("Form", formSchema);
+const Form = mongoose.model("Form", formSchema);
 
 // Auto assign password  
 // https: //www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
 // https: //stackoverflow.com/questions/44072750/how-to-send-basic-auth-with-axios
 // https://stackoverflow.com/questions/14559200/how-to-exclude-one-particular-field-from-a-collection-in-mongoose
 
-module.exports = Forms;
+module.exports = Form;
