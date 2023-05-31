a9bec18197b62718381c2dc521807b0b653267ab
Zhen <76132762+Zhfang01@users.noreply.github.com>
2023-05-31 14:35:48 -0700

    s

--- routes/api/ToDoList.js
+++ routes/api/ToDoList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//i want to develop to do list feature but i want to implement it after logging in. i dont know what to do
+// import ToDoList from './components/ToDoList';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 import ToDoList from './components/ToDoList';
 import Login from './components/Login';
-import ProtectedRoute from './components/ProtectedRoute';
+
 
 const App = () => (
     <Router>
