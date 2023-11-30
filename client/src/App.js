612febffcf458f7bdad79c2c9f83f749b02c5e16
nickcamp13 <nicholascampos013@gmail.com>
2023-11-30 14:31:28 -0800

    added theme provider to app

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,8 @@ import AssessmentPage from "./pages/AssessmentPage/index";
 import NavBar from "./components/NavBar";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from "@material-ui/core";
 
 import AdminPortal from "./pages/AccountPortal/versions/admin/AdminPortal";
 
@@ -36,6 +37,22 @@ import SocketContext from './socket-context'
 import * as io from 'socket.io-client'
 
 const socket = io()
+
+// Create a theme instance.
+const appTheme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'Nunito',
+      'Asap Condensed',
+      'sans-serif'
+    ].join(','),
+  },
+  palette: {
+    background: {
+      default: 'var(--background-color)'
+    }
+  },
+});
 
 function App() {
   // const testUser = null;
@@ -64,37 +81,38 @@ function App() {
   }, []);
 
   return (
-    <SocketContext.Provider value={socket}>
+    <ThemeProvider theme={appTheme}>
+      <SocketContext.Provider value={socket}>
 
-      <div className="App">
-        <header>
-          {/* Place navigation bar here */}
-          <NavBar user={userInfo} logout={handleLogout} />
-        </header>
+        <div className="App">
+          <header>
+            <NavBar user={userInfo} logout={handleLogout} />
+          </header>
 
-        {
-          loading ?
-            <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
-              <div style={{ margin: "auto" }}>
-                <FontAwesomeIcon icon={faSpinner} size="2x" spin />
+          {
+            loading ?
+              <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
+                <div style={{ margin: "auto" }}>
+                  <FontAwesomeIcon icon={faSpinner} size="2x" spin />
+                </div>
+
               </div>
+              :
+              <Switch>
+                {/* Portal component should check account type and render the correct component */}
+                <ProtectedRoute exact path="/" component={AccountPortal} user={userInfo} />
+                {/* Class component should check account type and render the correct component */}
+                <ProtectedRoute path="/subject/:id" component={SubjectPage} logout={handleLogout} user={userInfo} />
+                <ProtectedRoute path="/edit" component={AdminPortal} logout={handleLogout} user={userInfo} />
+                <Route exact path="/login" component={props => <LoginPortal {...props} user={userInfo} setUser={setUser} />} />
+                <Route path="/assessment/:id" component={AssessmentPage} logout={handleLogout} user={userInfo} />
+                <Route component={NoMatch} />
+              </Switch>
+          }
+        </div>
 
-            </div>
-            :
-            <Switch>
-              {/* Portal component should check account type and render the correct component */}
-              <ProtectedRoute exact path="/" component={AccountPortal} user={userInfo} />
-              {/* Class component should check account type and render the correct component */}
-              <ProtectedRoute path="/subject/:id" component={SubjectPage} logout={handleLogout} user={userInfo} />
-              <ProtectedRoute path="/edit" component={AdminPortal} logout={handleLogout} user={userInfo} />
-              <Route exact path="/login" component={props => <LoginPortal {...props} user={userInfo} setUser={setUser} />} />
-              <Route path="/assessment/:id" component={AssessmentPage} logout={handleLogout} user={userInfo} />
-              <Route component={NoMatch} />
-            </Switch>
-        }
-      </div>
-
-    </SocketContext.Provider>
+      </SocketContext.Provider>
+    </ThemeProvider>
   );
 }
 
