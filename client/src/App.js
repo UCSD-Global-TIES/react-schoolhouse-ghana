246c5eb59699ba4e26668c5beba76402b9171075
Bella Jeong <lahrry0925@gmail.com>
2023-12-03 20:07:49 -0800

    button changed

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,11 @@ import { Route, Switch } from "react-router-dom";
 
 // Material-UI Components and Styles
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import { CssBaseline } from "@material-ui/core";
 
 // Font and Icons
-import "typeface-roboto";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "typeface-roboto";
 
 // Socket
 import * as io from "socket.io-client";
@@ -29,15 +28,14 @@ import "./App.css";
 // Using Redirect https://reacttraining.com/react-router/web/api/Redirect
 import ProtectedRoute from "./components/ProtectedRoute";
 // import Loading from "./components/Loading"; // Commented out but kept for future reference
-import NavBar from "./components/NavBar";
 
 // Pages
-import NoMatch from "./pages/NoMatch/index";
-import LoginPortal from "./pages/LoginPortal/index";
 import AccountPortal from "./pages/AccountPortal/index";
-import SubjectPage from "./pages/SubjectPage/index";
-import AssessmentPage from "./pages/AssessmentPage/index";
 import AdminPortal from "./pages/AccountPortal/versions/admin/AdminPortal";
+import AssessmentPage from "./pages/AssessmentPage/index";
+import LoginPortal from "./pages/LoginPortal/index";
+import NoMatch from "./pages/NoMatch/index";
+import SubjectPage from "./pages/SubjectPage/index";
 
 const socket = io();
 
@@ -50,6 +48,29 @@ const appTheme = createMuiTheme({
   palette: {
     background: {
       default: "var(--background-color)",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        borderRadius: '20px', // Rounded corners
+        color: '#1976d2', // Blue text
+        backgroundColor: '#fff', // White background
+        padding: '8px 16px', // Padding around the text
+        textTransform: 'none', // Keeps the text's original casing
+        border: '1px solid #1976d2', // Blue border
+        boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)', // Shadow for the 3D effect
+        '&:hover': {
+          backgroundColor: '#f0f0f0', // Light grey background on hover
+          boxShadow: 'none', // No shadow on hover to give a pressed effect
+        },
+        '&:active': {
+          backgroundColor: '#e0e0e0', // Even lighter grey background to simulate being pressed
+        },
+      },
+      startIcon: {
+        color: '#1976d2', // Blue icon to match the text
+      },
     },
   },
 });
