a162c172f520148755f1bf2b1fedec8fbfc8dd2b
jel084 <130106981+jel084@users.noreply.github.com>
2024-10-16 11:06:56 -0700

    cleanedup files

--- client/src/components/ServerDash/index.js
+++ client/src/components/ServerDash/index.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { TextField } from "@material-ui/core";
-import FilesForm from "../FilesForm";
-import DocumentEditor from "../DocumentEditor";
-import { makeStyles } from '@material-ui/core/styles';
+// import React, { useEffect, useState } from "react";
+// import { TextField } from "@material-ui/core";
+// import FilesForm from "../FilesForm";
+// import DocumentEditor from "../DocumentEditor";
+// import { makeStyles } from '@material-ui/core/styles';
 
-import "../../utils/flowHeaders.min.css";
-import { faFile } from "@fortawesome/free-solid-svg-icons";
+// import "../../utils/flowHeaders.min.css";
+// import { faFile } from "@fortawesome/free-solid-svg-icons";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-    }
-}));
+// const useStyles = makeStyles(theme => ({
+//     root: {
+//         display: 'flex',
+//     }
+// }));
 
 
-function ServerDash(props) {
+// function ServerDash(props) {
 
-    useEffect(() => {
+//     useEffect(() => {
 
-    }, []);
+//     }, []);
 
-    return (
-        <>
-            <DocumentEditor
-                primary={'title'}
-                collection={'Files'}
-                icon={faFile}
-                FormComponent={FilesForm}
-                {...props} />
-        </>
-    )
-};
+//     return (
+//         <>
+//             <DocumentEditor
+//                 primary={'title'}
+//                 collection={'Files'}
+//                 icon={faFile}
+//                 FormComponent={FilesForm}
+//                 {...props} />
+//         </>
+//     )
+// };
 
-export default ServerDash;
+// export default ServerDash;
--- client/src/components/UserList/UserList.jsx
+++ client/src/components/UserList/UserList.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-import NameCard from '../NameCard/NameCard';
+// import React from 'react';
+// import { makeStyles } from "@material-ui/core/styles";
+// import NameCard from '../NameCard/NameCard';
 
-const useStyles = makeStyles({
-  header: {
-    color: "#4B4B4B",
-    fontFamily: "Asap Condensed",
-    fontSize: "2.5rem",
-    fontStyle: "normal",
-    fontWeight: "bold",
-    lineHeight: "4rem" /* 160% */
-  }
-})
+// const useStyles = makeStyles({
+//   header: {
+//     color: "#4B4B4B",
+//     fontFamily: "Asap Condensed",
+//     fontSize: "2.5rem",
+//     fontStyle: "normal",
+//     fontWeight: "bold",
+//     lineHeight: "4rem" /* 160% */
+//   }
+// })
 
-const UserList = ({userCategory, users}) => {
-  const classes = useStyles();
+// const UserList = ({userCategory, users}) => {
+//   const classes = useStyles();
 
-  return(
-    <div>
-      <h2 className={classes.header}>{userCategory}</h2>
-      {users.map((user) => {
-        return <NameCard name={user}/>
-      })}
-    </div>
-  )
-}
-export default UserList;
+//   return(
+//     <div>
+//       <h2 className={classes.header}>{userCategory}</h2>
+//       {users.map((user) => {
+//         return <NameCard name={user}/>
+//       })}
+//     </div>
+//   )
+// }
+// export default UserList;
