e1f8b98d80c5a58bfca24588ae7cf67270c7653c
jel084 <130106981+jel084@users.noreply.github.com>
2025-04-21 15:24:03 -0700

    updated permissions for teacher

--- client/src/components/AnnouncementCard/AnnouncementCard.js
+++ client/src/components/AnnouncementCard/AnnouncementCard.js
@@ -132,7 +132,7 @@ function AnnouncementCard(props) {
             CREATED ON: {moment(PROPS.document.createdAt).format('MM/DD/YYYY')}
           </p>
         </div>
-        {user && user.type === "Admin" && (
+        {user && (user.type === "Admin" || user.type === "Teacher") && (
           <div className={classes.iconContainer}>
             <div className={classes.icon} onClick={handleClick}>
               <svg
